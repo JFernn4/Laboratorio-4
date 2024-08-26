4ccd495ee55555cc3a0bc2218336fad8f32b8e29
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-08-26 00:14:51 -0600

    Menu

--- Laboratorio 4/Program.cs
+++ Laboratorio 4/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool menu = true;
+int opcion;
+while (menu)
+{
+    MostrarMenu();
+    opcion= Convert.ToInt32(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            {
+                break;
+            }
+        case 2:
+            {
+                break;
+            }
+        case 3:
+            {
+                break;
+            }
+        case 4:
+            {
+                break;
+            }
+        case 5:
+            {
+                break;
+            }
+        case 6:
+            {
+                menu = false;
+                break;
+            }
+    }
+}
+static void MostrarMenu()
+{
+    Console.Clear();
+    Console.WriteLine("1. Agregar libro.");
+    Console.WriteLine("2. Registrar usuario.");
+    Console.WriteLine("3. Catálogo.");
+    Console.WriteLine("4. Buscar libro.");
+    Console.WriteLine("5. Prestar libro.");
+    Console.WriteLine("6. Salir.");
+}
