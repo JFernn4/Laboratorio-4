9d449a0df43245d5c7435899db4d666398b00d98
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-08-26 00:02:50 -0600

    Clases de clientes

---
+++ Laboratorio 4/ClienteVIP.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_4
+{
+    internal class ClienteVIP : Cliente
+    {
+        public ClienteVIP(int iD, string nombre, string metodoPago) : base(iD, nombre, metodoPago)
+        {
+        }
+    }
+}
